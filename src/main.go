3c46a0c8a2d1634622751f2c4861278ac15eca41
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-01-15 16:46:15 -0300

    feat: use middleware and routes Use error handling middleware, auth and users routes

--- src/main.go
+++ src/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/MaxiOtero6/go-auth-rest/database"
-
+	"github.com/MaxiOtero6/go-auth-rest/middleware"
+	"github.com/MaxiOtero6/go-auth-rest/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -19,4 +22,16 @@ func main() {
 	loadEnv()
 
 	database.ConnectDB()
+
+	router := gin.Default()
+
+	router.Use(middleware.ErrorHandler())
+	routes.AuthRoutes(router)
+	routes.UsersRoutes(router)
+
+	host := os.Getenv("HOST")
+	port := os.Getenv("PORT")
+	addr := host + ":" + port
+
+	router.Run(addr)
 }
