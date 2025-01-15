fa848a95608d5c8d72c2febcfd99cfa373efc15f
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-01-15 16:45:40 -0300

    feat: hash user password

---
+++ src/service/users.go
@@ -0,0 +1,16 @@
+package service
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/MaxiOtero6/go-auth-rest/model"
+	"github.com/MaxiOtero6/go-auth-rest/repository"
+)
+
+func CreateUser(userData *model.BaseUser) model.User {
+	hashPasswordBytes := sha256.Sum256([]byte(userData.Password))
+	userData.Password = hex.EncodeToString(hashPasswordBytes[:])
+
+	return repository.CreateUser(userData)
+}
