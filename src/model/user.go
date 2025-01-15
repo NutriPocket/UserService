832d6a72927c617041ac22214e751cdf1b988d3f
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2025-01-15 16:44:36 -0300

    feat: user model

---
+++ src/model/user.go
@@ -0,0 +1,12 @@
+package model
+
+type BaseUser struct {
+	Username string
+	Email    string
+	Password string
+}
+
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
