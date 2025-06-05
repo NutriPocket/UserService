f459aafcd4918d9dc8e01f6c963c075879f26f42
Maximiliano Otero <106620067+MaxiOtero6@users.noreply.github.com>
2025-06-05 19:17:50 -0400

    feat: user picture (#5)

--- src/model/user.go
+++ src/model/user.go
@@ -8,11 +8,16 @@ type BaseUser struct {
 	Password string
 }
 
+type EditableUser struct {
+	Picture string `json:"picture"`
+}
+
 // User is a struct that contains the user data that will be sent to the client
 type User struct {
 	Id       string `json:"id"`
 	Username string `json:"username"`
 	Email    string `json:"email"`
+	EditableUser
 }
 
 // LoginUser is a struct that contains the user data received from the client when logging in
